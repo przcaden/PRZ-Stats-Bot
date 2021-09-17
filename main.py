148c41f5b9c8bf97c7b73fabd66f0d1a5a53ad12
Caden Perez <77550338+przcaden@users.noreply.github.com>
2021-09-17 04:10:45 +0000

    Began writing message stats command

--- main.py
+++ main.py
@@ -1,19 +1,44 @@
 import discord
+from discord.ext import commands, tasks
 import os
 
-client = discord.Client()
+bot = commands.Bot(command_prefix = '$')
 token = os.environ['.env']
 
-@client.event
+# runs when the bot comes online
+@bot.event
 async def on_ready():
-  print('Logged in as {0.user}'.format(client))
+  print('Logged in as {0.user}'.format(bot))
 
-@client.event
-async def on_message(msg): # on message received
-  if msg.author == client.user:
+# generates message stats on all users or a single one.
+@bot.command()
+async def msgstats(ctx, arg): 
+  print('Entering msgstats configuration...')
+  if arg.lower() == 'all':
+    users = ctx.guild.members # list of all users in server
+    counters = [0] * len(users) # list of message counters
+    for channel in ctx.guild.channels:
+      for msg in channel.history(limit=None):
+        for u in users:
+          if msg.author == u:
+            counters[users.index(u)] += 1
+  else:
+    if ctx.guild.members.contains(arg):
+      counter = 0
+      for channel in ctx.guild.channels:
+        for msg in channel.history(limit=None):
+          if msg.author == arg:
+            counter += 1
+    await ctx.send('User {arg} has sent {counter} messages in this server.'.format(arg, counter))
+
+
+# runs when a message is sent. checks for numerous commands.
+@bot.event
+async def on_message(msg):
+  if msg.author == bot.user:
     return
-
-  if msg.content.startswith('ping') or msg.content.startswith('PING') or msg.content.startswith('ping.'):
+  if msg.content.lower() == 'ping': # ping pong!
     await msg.channel.send('pong')
+  await bot.process_commands(msg)
 
-client.run(token)
+bot.run(token)

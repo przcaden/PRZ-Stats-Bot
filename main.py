05f4d673907e1c3240beb4d3726c71d5d693ddab
Caden Perez <77550338+przcaden@users.noreply.github.com>
2021-09-17 02:25:15 +0000

    Basic run prep and message checking

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
 import discord
+import os
+
+client = discord.Client()
+token = os.environ['.env']
+
+@client.event
+async def on_ready():
+  print('Logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(msg): # on message received
+  if msg.author == client.user:
+    return
+
+  if msg.content.startswith('ping') or msg.content.startswith('PING') or msg.content.startswith('ping.'):
+    await msg.channel.send('pong')
+
+client.run(token)

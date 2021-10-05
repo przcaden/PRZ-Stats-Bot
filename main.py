bfbfbdf82ab2a8c0c389373bb348149a6d02e14b
Caden Perez <77550338+przcaden@users.noreply.github.com>
2021-10-05 20:27:34 +0000

    writing all stats algorithm

--- main.py
+++ main.py
@@ -10,26 +10,40 @@ token = os.environ['.env']
 async def on_ready():
   print('Logged in as {0.user}'.format(bot))
 
+@bot.command()
+async def pong(ctx):
+  print('in pong')
+  await ctx.send('ping')
+
 # generates message stats on all users or a single one.
 @bot.command()
-async def msgstats(ctx, arg): 
-  print('Entering msgstats configuration...')
-  if arg.lower() == 'all':
-    users = ctx.guild.members # list of all users in server
-    counters = [0] * len(users) # list of message counters
-    for channel in ctx.guild.channels:
-      for msg in channel.history(limit=None):
-        for u in users:
-          if msg.author == u:
-            counters[users.index(u)] += 1
-  else:
-    if ctx.guild.members.contains(arg):
-      counter = 0
-      for channel in ctx.guild.channels:
-        for msg in channel.history(limit=None):
-          if msg.author == arg:
-            counter += 1
-    await ctx.send('User {arg} has sent {counter} messages in this server.'.format(arg, counter))
+# Retrieve total amount of messages user has sent
+async def msgstats(ctx, arg):
+    print('Entering msgstats configuration...')
+    if arg.lower() == 'all':
+        await ctx.send("Calculating message stats for all server users. May take several moments...")
+        users = ctx.guild.members # list of all users in server
+        counters = [0] * len(users) # list of message counters
+        for channel in ctx.guild.channels:
+            for msg in channel.history(limit=None):
+                for u in users:
+                    if msg.author == u:
+                        counters[users.index(u)] += 1
+        msg = ""
+        counter = 0
+        for user in users:
+            msg += "User " + user + " has " + counters[user.index] + " messages in the server.\n"
+            counter += counters[user.index]
+        msg += "There are a total of " + counter + " messages in this server."
+        await ctx.send(msg)
+    else:
+        if ctx.guild.members.contains(arg):
+            counter = 0
+            for channel in ctx.guild.channels:
+                for msg in channel.history(limit=None):
+                    if msg.author == arg:
+                        counter += 1
+        await ctx.send('User {arg} has sent {counter} messages in this server.'.format(arg, counter))
 
 
 # runs when a message is sent. checks for numerous commands.
